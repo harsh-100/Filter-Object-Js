d9724b718401c23679ec8f187f5cf230e4153459
Developer0Rohit <rkdjsoundrewasa1234@gmail.com>
2023-11-07 10:07:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const categorySelect = document.getElementById('category');
+    const items = document.querySelectorAll('.item');
+  
+    categorySelect.addEventListener('change', function() {
+      const selectedCategory = categorySelect.value;
+  
+      items.forEach(function(item) {
+        if (selectedCategory === 'all' || item.classList.contains(selectedCategory)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    });
+  });
