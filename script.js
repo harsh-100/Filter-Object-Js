ec2df1326913bcb6822e132910ce27f57759f7ea
Reactongraph <js.developer@ongraph.com>
2024-02-29 20:29:29 +0530

    fix:adding comment

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-window.addEventListener('DOMContentLoaded', function() {
-    const categorySelect = document.getElementById('category');
-    const items = document.querySelectorAll('.item');
-  
-    categorySelect.addEventListener('change', function() {
-      const selectedCategory = categorySelect.value;
-  
-      items.forEach(function(item) {
-        if (selectedCategory === 'all' || item.classList.contains(selectedCategory)) {
-          item.style.display = 'flex';
-        } else {
-          item.style.display = 'none';
-        }
-      });
+window.addEventListener("DOMContentLoaded", function () {
+  const categorySelect = document.getElementById("category");
+  const items = document.querySelectorAll(".item");
+
+  categorySelect.addEventListener("change", function () {
+    const selectedCategory = categorySelect.value;
+
+    items.forEach(function (item) {
+      if (
+        selectedCategory === "all" ||
+        item.classList.contains(selectedCategory)
+      ) {
+        item.style.display = "flex";
+      } else {
+        item.style.display = "none";
+      }
     });
   });
+});
+// adding a comment for trial
